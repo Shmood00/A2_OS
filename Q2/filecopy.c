44467004084c9fd6d4dcf06c78f65cf27ea1d77e
Shmood00 <43577426+Shmood00@users.noreply.github.com>
2018-10-07 17:51:04 -0400

    Added error checking to program.

--- Q2/filecopy.c
+++ Q2/filecopy.c
@@ -6,18 +6,24 @@
 
 /*
 Adam Iannaci, Ian Gerics
+
 */
 
 int main(int argc, char *argv[]){
 
         int readWrite[2];
-        pid_t child; 
+        pid_t child;
         FILE *file, *file2;
         char readBuffer[100000];
         char fileBuffer[100000]; 
 
+	if((argc-1) != 2){
+		fprintf(stderr, "%s", "Error, must enter in 2 arguments.\n");
+		fprintf(stderr, "%s", "Usage: filecopy arg1 arg2.\n");
+		exit(1);
+	}
 
-        pipe(readWrite); 
+        pipe(readWrite);
 
         child = fork();
 
@@ -27,32 +33,40 @@ int main(int argc, char *argv[]){
         }
 
         if(child == 0){
-                
-
-                close(readWrite[0]); 
+               
+                close(readWrite[0]);
                 
                 file = fopen(argv[1], "r");
-	
+		
+		if(file == NULL){
+			fprintf(stderr, "%s", "File not found.\n");
+		}else{
 
-		while(fgets(fileBuffer, sizeof(fileBuffer), file) != NULL){
+		        while(fgets(fileBuffer, sizeof(fileBuffer), file) != NULL){
 
-			write(readWrite[1], fileBuffer, strlen(fileBuffer)); 
+		                write(readWrite[1], fileBuffer, strlen(fileBuffer));
+
+		        }
+
+		        fclose(file);
 		}
 
-		fclose(file);
-
         }else{
-                
-                close(readWrite[1]); 
+              
+                close(readWrite[1]);
                 
 		file2 = fopen(argv[2], "w");
 		
+		if(file2 == NULL){
+			fprintf(stderr, "%s", "Error opening file.\n");		
+		}else{
 		               
-		read(readWrite[0], readBuffer, sizeof(readBuffer));
+			read(readWrite[0], readBuffer, sizeof(readBuffer));
 
-		fprintf(file2, "%s", readBuffer);
+			fprintf(file2, "%s", readBuffer);
 				
-		fclose(file2);
+			fclose(file2);
+		}
 
         }
 
