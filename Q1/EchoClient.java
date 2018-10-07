7235165891a45b70dfbd262be64f436b7d010111
ian <IanGerics>
2018-10-07 15:05:50 -0700

    Echo Server and Client

--- Q1/EchoClient.java
+++ Q1/EchoClient.java
@@ -1,33 +1,35 @@
+/* Ian Gerics and Adam Iannaci*/
+
 import java.io.*;
 import java.net.*;
  
 public class EchoClient {
     public static void main(String[] args) throws Exception {
- 	
-        if (args.length != 1) {
+         
+        if (args.length != 1) { //If the expected use is not given, exit and print message
             System.err.println("Usage: java EchoClient <host name>");
             System.exit(1);
         }
-
+ 
         String hostName = args[0];
         int portNumber = 44288;
  
-        try (
+        try ( // Try to create socket and buffers
             Socket echoSock = new Socket(hostName, portNumber);
             PrintWriter prOut = new PrintWriter(echoSock.getOutputStream(), true);
             BufferedReader bufIn = new BufferedReader(new InputStreamReader(echoSock.getInputStream()));
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
         ) {
             String userInput;
-	    String exit = ".";
-            while ((userInput = stdIn.readLine()) != null) {
+			String exit = ".";
+            while ((userInput = stdIn.readLine()) != null) { // read line and send to echo server 
                 prOut.println(userInput);
-			if(userInput.equals(exit)){
-				System.exit(1);
-			}
-                System.out.println("Server: " + bufIn.readLine());
+				if(userInput.equals(exit)){
+					System.exit(1);
+				}
+                System.out.println("Server: " + bufIn.readLine()); // Print reply
             }
-        } catch (Exception e) {
+        } catch (Exception e) { // Throw exception if something does wrong
             System.err.println("Something went wrong");
 			System.out.println(e.getMessage());
         }
--- Q1/EchoServer.java
+++ Q1/EchoServer.java
@@ -1,27 +1,30 @@
+/* Ian Gerics and Adam Iannaci*/
+
+
 import java.net.*;
 import java.io.*;
  
 public class EchoServer {
     public static void main(String[] args) throws Exception {
-	
-	if (args.length != 0) {
+         
+        if (args.length != 0) { // If the expected use is not given, exit and print message
             System.err.println("Usage: java EchoServer");
             System.exit(1);
         }
-
+         
         int portNumber = 44288;
          
-        try (
+        try ( // Try to create socket and buffers
             ServerSocket srvSock = new ServerSocket(portNumber); 
             Socket cliSock = srvSock.accept();     
             PrintWriter prOut = new PrintWriter(cliSock.getOutputStream(), true);                   
             BufferedReader bufIn = new BufferedReader(new InputStreamReader(cliSock.getInputStream()));
         ) {
             String inLine;
-            while ((inLine = bufIn.readLine()) != null) {
+            while ((inLine = bufIn.readLine()) != null) { // Read line from client
                 prOut.println(inLine);
             }
-        } catch (Exception e) {
+        } catch (Exception e) { // Throw exception if something goes wrong
             System.out.println("Exception caught when trying to run");
             System.out.println(e.getMessage());
         }
